046af6fda39f91cc1c417bc945d7ba7b8b05935f
yiomurillo <jordimurillo93@gmail.com>
2022-01-19 23:40:23 -0500

    Ajustes en back 11:40 19/01/22

--- back/ms_tech_test/src/main/java/com/test/octans/controller/RolController.java
+++ back/ms_tech_test/src/main/java/com/test/octans/controller/RolController.java
@@ -3,10 +3,15 @@ package com.test.octans.controller;
 import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientResponseException;
 
 import com.test.octans.dto.RolDto;
 import com.test.octans.service.RolService;
@@ -17,9 +22,21 @@ public class RolController {
 
 	@Autowired
 	private RolService rolService;
-	
-	@GetMapping
-	public ResponseEntity<List<RolDto>> getAllRoles(){
+
+	@GetMapping("consultar")
+	@CrossOrigin
+	public ResponseEntity<List<RolDto>> getAllRoles() {
 		return ResponseEntity.ok(rolService.findAll());
+	}
+
+	@PostMapping("guardar")
+	@CrossOrigin
+	public ResponseEntity<RolDto> saveUsuario(@RequestBody RolDto rol) {
+		try {
+			return new ResponseEntity<>(rolService.save(rol), HttpStatus.OK);
+		} catch (RestClientResponseException e) {
+			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
+		}
+
 	}
 }
--- back/ms_tech_test/src/main/java/com/test/octans/controller/UsuarioController.java
+++ back/ms_tech_test/src/main/java/com/test/octans/controller/UsuarioController.java
@@ -1,10 +1,11 @@
 package com.test.octans.controller;
 
-import java.net.URI;
 import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.DeleteMapping;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
@@ -12,6 +13,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestClientResponseException;
 
 import com.test.octans.dto.UsuarioDto;
 import com.test.octans.service.UsuarioService;
@@ -19,33 +21,35 @@ import com.test.octans.service.UsuarioService;
 @RestController
 @RequestMapping("/usuario/")
 public class UsuarioController {
-	
+
 	@Autowired
 	private UsuarioService usuarioService;
-	
+
 	@GetMapping("consultar")
-	public ResponseEntity<List<UsuarioDto>> getAllUsuarios(){
+	@CrossOrigin
+	public ResponseEntity<List<UsuarioDto>> getAllUsuarios() {
 		return ResponseEntity.ok(usuarioService.findAll());
 	}
-	
+
 	@PostMapping("guardar")
-	public ResponseEntity<UsuarioDto> saveUsuario(@RequestBody UsuarioDto usuario){
-		
+	@CrossOrigin
+	public ResponseEntity<UsuarioDto> saveUsuario(@RequestBody UsuarioDto usuario) {
+
 		try {
-			UsuarioDto userSave = usuarioService.save(usuario);
-			return ResponseEntity.created(new URI("/usuario/"+userSave.getNombre())).body(userSave);
-		} catch (Exception e) {
+			return new ResponseEntity<>(usuarioService.save(usuario), HttpStatus.OK);
+		} catch (RestClientResponseException e) {
 			return ResponseEntity.status(HttpStatus.BAD_REQUEST).build();
 		}
-		
+
 	}
-	
+
 	@DeleteMapping(value = "delete/{id}")
-	public ResponseEntity<Boolean> deleteUsuario(@PathVariable("id") Long id){
+	@CrossOrigin
+	public ResponseEntity<Boolean> deleteUsuario(@PathVariable("id") Long id) {
 		Boolean resp = false;
 		usuarioService.deleteById(id);
 		resp = true;
-		
+
 		return ResponseEntity.ok(resp);
 	}
 }
